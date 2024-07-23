e39572f91cb388aa5530862fbbde3894ccf30948
DhruvilAndropedia <dhruvil@devandro.in>
2024-07-23 14:22:08 +0530

    - changes

--- src/constant/baseUrl.ts
+++ src/constant/baseUrl.ts
@@ -1,6 +1,5 @@
-export const BASE_URL = `https://api.localfirst.in`;
-export const IMAGE_BASE_URL = `https://localfirst-gallery.s3.ap-south-1.amazonaws.com`;
-// export const BASE_URL = `https://localfirstapi.uatdev.site`;
+export const BASE_URL = `https://apapi.localfirst.in`;
+// export const BASE_URL = `https://test-localfirst.devteamandro.co.in`;
 
 export const DELIVERY_PERSON = `/deliveryPerson`;
 export const DELIVERY = `/delivery`;
